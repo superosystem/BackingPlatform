cadf693f45ab781ae37a6bcffa28a4003d6f356f
gusrylmubarok <gusrylmubarok@gmail.com>
2022-02-05 10:30:32 +0700

    Relasi Entity dengan JPA

--- src/main/java/com/gusrylmubarok/demoapi/models/entity/Product.java
+++ src/main/java/com/gusrylmubarok/demoapi/models/entity/Product.java
@@ -1,18 +1,23 @@
 package com.gusrylmubarok.demoapi.models.entity;
 
 import java.io.Serializable;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.validation.constraints.NotEmpty;
 
 
 @Entity
-@Table(name = "tbl_product")
+@Table(name = "tbl_products")
 public class Product implements Serializable {
     
     private static final long serialVersionUID = 1L;
@@ -30,6 +35,16 @@ public class Product implements Serializable {
     private String description;
 
     private double price;
+
+    @ManyToOne
+    private Category category;
+
+    @ManyToMany
+    @JoinTable(
+        name = "tbl_product_supplier", 
+        joinColumns = @JoinColumn(name = "product_id"),
+        inverseJoinColumns = @JoinColumn(name = "supplier_id"))
+    private Set<Supplier> suppliers;
 
     public Product() {
     }
@@ -71,6 +86,22 @@ public class Product implements Serializable {
 
     public void setPrice(double price) {
         this.price = price;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Set<Supplier> getSuppliers() {
+        return suppliers;
+    }
+
+    public void setSuppliers(Set<Supplier> suppliers) {
+        this.suppliers = suppliers;
     }
 
 }
