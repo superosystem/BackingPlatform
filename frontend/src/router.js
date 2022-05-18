46492c42ea03a7b4134e124c07f91380b01e2097
asmpewpew@gmail.com <mrpacman79>
2022-05-18 12:18:53 +0700

    Deploy FrontEnd Fitur

---
+++ frontend/src/router.js
@@ -0,0 +1,26 @@
+import { createWebHistory, createRouter } from "vue-router";
+
+const routes = [
+    {
+        path: "/",
+        alias: "/tutorials",
+        name: "tutorials",
+        component: () => import("./components/TutorialsList.vue")
+    },
+    {
+        path: "/tutorials/:id",
+        name: "tutorials-details",
+        component: () => import("./components/Tutorial.vue")
+    },
+    {
+        path: "/add",
+        name: "add",
+        component: () => import("./components/AddTutorial.vue")
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+export default router;
