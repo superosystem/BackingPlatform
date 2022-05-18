f6720faec7ab209ca776f7265a430bc179577f46
Gusryl Mubarok <gusrylmubarok@gmail.com>
2022-05-18 23:12:18 +0700

    Push Back V1.0

---
+++ backend/src/main/java/com/gusrylmubarok/stripespeed/StripeSpeedApplication.java
@@ -0,0 +1,13 @@
+package com.gusrylmubarok.stripespeed;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StripeSpeedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StripeSpeedApplication.class, args);
+	}
+
+}
