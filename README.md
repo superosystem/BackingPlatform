139c5dc8dded4805930d84878f9e3b5135c6668e
Gusryl Mubarok <gusrylmubarok@gmail.com>
2022-05-18 23:04:01 +0700

    Clean Repository

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Spring Boot Demo
+# Stripe Speed
--- src/main/java/com/gusrylmubarok/spring/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.gusrylmubarok.spring;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-}
