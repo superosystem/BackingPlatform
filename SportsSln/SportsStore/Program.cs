f8450a04d96dfc9e070ef45bf8081b8a30280191
Agus S. Mubarok <gusrylmubarok@gmail.com>
2022-09-21 10:00:38 +0700

    Upload First View SportsShop

--- SportsSln/SportsStore/Program.cs
+++ SportsSln/SportsStore/Program.cs
@@ -1,6 +1,24 @@
+using Microsoft.EntityFrameworkCore;
+using SportsStore.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<StoreDbContext>(opts =>
+{
+    opts.UseSqlServer(builder.Configuration["ConnectionStrings:SportsStoreConnection"]);
+});
+builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// app.MapGet("/", () => "Hello World!");
+app.UseStaticFiles();
+app.MapControllerRoute("pagination",
+    "Products/Page{productPage}",
+    new { Controller = "Home", action = "Index" });
+app.MapDefaultControllerRoute();
+
+SeedData.EnsurePopulated(app);
 
 app.Run();
